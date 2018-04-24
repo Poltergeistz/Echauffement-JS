a76cd9ca2e454b66e50b6a67d581e4adb8bdd8ab
DavidJ <38471754+Poltergeistz@users.noreply.github.com>
2018-04-24 16:10:24 +0200

    Add files via upload

---
+++ Echauffement 2.js
@@ -0,0 +1,7 @@
+var a, d
+
+while (! a) a = prompt ("De quelle couleur est le cheval blanc d'Henry 4?")
+while (! d) d = prompt ("Combien y'a t il de 7 nains ?")
+
+var blanc = "Bravo tu as trouvé la réponse"
+var nains = "Bravo"
