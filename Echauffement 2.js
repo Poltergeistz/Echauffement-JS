446fbf6d4b4174a2cd362a0f7be5e9e2277f955b
DavidJ <38471754+Poltergeistz@users.noreply.github.com>
2018-04-24 16:57:48 +0200

    Update Echauffement 2.js

--- Echauffement 2.js
+++ Echauffement 2.js
@@ -2,6 +2,9 @@ var a, d
 
 while (! a) a = prompt ("De quelle couleur est le cheval blanc d'Henry 4?")
 while (! d) d = prompt ("Combien y'a t il de 7 nains ?")
-
-var blanc = "Bravo tu as trouvé la réponse"
-var nains = "Bravo"
+var x = blanc && 7
+// true is blanc et 7 seulement blanc et 7
+return ("Bravo!")
+else
+// return false si les reponses ne sont pas blanc en 7
+return ("Essaye encore")
