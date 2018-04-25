dc4922feaae2d1ff5d42fe452151d83f5afd43d8
DavidJ <38471754+Poltergeistz@users.noreply.github.com>
2018-04-25 10:55:22 +0200

    Update Echauffement 1.js

--- Echauffement 1.js
+++ Echauffement 1.js
@@ -1,10 +1,17 @@
-function Comment(){
-var prenom = prompt("Comment tu t'appeles ?");
-alert ("Bonjour," + prenom);
-//NombreDeCaracteres à ne pas dépasser est 10
-  if (prenom >= 10)}
-  return prompt ("Bonjour," + prenom){
-  }
-else (prenom <= 10){
-console.log ("Bonjour," + prenom)
+function Question (){
+// Crée une boucle en posant la question
+var prenom = prompt ("Comment tu t'appeles ?");
+  // Pose la question en popup : Comment tu t'appeles ?
+if (prenom.length>1 && prenom.length<=10){
+  // Donne une valeur de caracteres comprise entre 1 et 10 ou égale à 10. Si cette valeur est comprise entre 1 et/ou égale à 10.
+return ("Bonjour " + prenom)
+  //Affiche Bonjour + la variable prenom qui est n'importe quel prenom compris entre 1 et 10 ou egal à 10
+
+}else{
+// Sinon 
+return (Question());
+  // retourne à la boucle fonction pour reposer la question vu que le nombre de caractere excède 10
 }
+}
+alert (Question())
+// Affiche la fonction
