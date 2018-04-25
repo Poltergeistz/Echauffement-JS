e307e79b8d9661537b2418e3ec6c1025c041b7fd
DavidJ <38471754+Poltergeistz@users.noreply.github.com>
2018-04-25 11:33:27 +0200

    Update Echauffement 2.js

--- Echauffement 2.js
+++ Echauffement 2.js
@@ -1,6 +1,6 @@
 function Question1(){
   // Créé la fonction Question1
-var couleur = prompt ("De quelle couleur est le cheval blanc d'Henry 4?")
+var couleur = prompt ("De quelle couleur est le cheval blanc d'Henry 4?");
 // Crée une variable couleur avec compris minuscules et majuscules > Pose la question1 en popup De quelle couleur est le cheval blanc d'Henry 4?
 if (couleur === blanc || couleur === BLANC){
   // Si le cheval est écrit en BLANC - blanc - Blanc alors
@@ -8,7 +8,7 @@ if (couleur === blanc || couleur === BLANC){
   // Félicite Bravo, question suivante...
   return (Question2());
   // Affiche la question suivante
-else{
+}else{
   // Sinon
   alert ("Essaye encore!")
   // Annonce Essaye encore!
@@ -16,15 +16,16 @@ return (Question1());
 // retourne à la fonction Question1
 }
 }
+
 function Question2(){
   // Créé la fonction Question2
-var nombre = prompt ("Combien y'a t il de 7 nains ?")
+var nombre = prompt ("Combien y'a t il de 7 nains ?");
 // Créé la variable nombre > pose la Question2 en popup Combien y'a t il de 7 nains ?
-if (nombre === 7)
+if (nombre === 7){
 // Si nombre est strictement égal à 7
   return ("Bravo! Tu as répondu à toutes les questions");
   // Alors Félicite : Bravo tu as répondu à toutes les questions !
-else{
+}else{
   // Sinon
 alert ("Essaye encore!")
 // Annonce Essaye encore !
@@ -32,3 +33,4 @@ return (Question2());
 // retourne à la fonction Question2
 }
 }
+
