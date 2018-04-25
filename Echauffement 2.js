dc903d9aaa33f9b99670cf33a91f0563c51bb345
DavidJ <38471754+Poltergeistz@users.noreply.github.com>
2018-04-25 12:02:54 +0200

    Update Echauffement 2.js

--- Echauffement 2.js
+++ Echauffement 2.js
@@ -1,9 +1,10 @@
 function Question1(){
   // Créé la fonction Question1
-var couleur = prompt ("De quelle couleur est le cheval blanc d'Henry 4?");
+var reponse = prompt ("De quelle couleur est le cheval blanc d'Henry 4?");
 // Crée une variable couleur avec compris minuscules et majuscules > Pose la question1 en popup De quelle couleur est le cheval blanc d'Henry 4?
-if (couleur === blanc || couleur === BLANC){
-  // Si le cheval est écrit en BLANC - blanc - Blanc alors
+if (reponse.toLowerCase() === 'blanc'){
+  // Si reponse est écrit en BLANC - blanc - Blanc alors transforme en blanc (minuscules)
+  console.log (reponse)
   alert ("Bravo, question suivante...")
   // Félicite Bravo, question suivante...
   return (Question2());
@@ -21,8 +22,8 @@ function Question2(){
   // Créé la fonction Question2
 var nombre = prompt ("Combien y'a t il de 7 nains ?");
 // Créé la variable nombre > pose la Question2 en popup Combien y'a t il de 7 nains ?
-if (nombre === 7){
-// Si nombre est strictement égal à 7
+if (nombre == 7){
+// Si nombre est égal à 7
   return ("Bravo! Tu as répondu à toutes les questions");
   // Alors Félicite : Bravo tu as répondu à toutes les questions !
 }else{
@@ -33,4 +34,4 @@ return (Question2());
 // retourne à la fonction Question2
 }
 }
-
+Question1()
