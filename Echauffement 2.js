289605059bdfacd249bd0e4d9ad0ccda40ebc1dd
DavidJ <38471754+Poltergeistz@users.noreply.github.com>
2018-04-25 11:10:26 +0200

    Update Echauffement 2.js

--- Echauffement 2.js
+++ Echauffement 2.js
@@ -2,18 +2,20 @@ function Question1()
 var a = blanc
 var b =
 prompt ("De quelle couleur est le cheval blanc d'Henry 4?")
-if (a){
+if (a)
+  return (Question2())
 }
 else (b){
 }
-return ("Bien joué!")
+return (Question1())
 
 function Question2
 var c = 7
-prompt ("Combien y'a t il de 7 nains ?")
 var d = 
+prompt ("Combien y'a t il de 7 nains ?")
 if (c)
+  return ("Bravo!")
 {else (b)
 }
-return 
+return (Question2())
 
