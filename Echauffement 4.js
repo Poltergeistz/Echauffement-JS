450684c7dd8296e790cafbff7455da946ee724ee
DavidJ <38471754+Poltergeistz@users.noreply.github.com>
2018-04-25 15:25:47 +0200

    Update Echauffement 4.js

--- Echauffement 4.js
+++ Echauffement 4.js
@@ -1,5 +1,14 @@
 function Random (min, max){
-  var random = Math.floor(Math.random()*101);
-return random
+  var random = Math.floor(Math.random()*(max - min));
 }
+function RandomOperator(min,max){
+  var operator = Math.floor(Math.random()*(max - min))
+  if (operator === 0){
+    return "+"
+  }
+  else if(operator === 1){
+    return "-"
+  }
+  else if(operator ===2){
+    
 Random()
