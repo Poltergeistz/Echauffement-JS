4938b49dbd96d476dfb9e5431bd3b26f62690104
DavidJ <38471754+Poltergeistz@users.noreply.github.com>
2018-04-25 10:59:20 +0200

    Update Echauffement 1.js
    comment

--- Echauffement 1.js
+++ Echauffement 1.js
@@ -1,7 +1,7 @@
 function Question (){
-// Crée une boucle en posant la question
+// Crée une fonction Question
 var prenom = prompt ("Comment tu t'appeles ?");
-  // Pose la question en popup : Comment tu t'appeles ?
+  // Crée une variable prenom avec aucune valeur > Pose la question en popup : Comment tu t'appeles ?
 if (prenom.length>1 && prenom.length<=10){
   // Donne une valeur de caracteres comprise entre 1 et 10 ou égale à 10. Si cette valeur est comprise entre 1 et/ou égale à 10.
 return ("Bonjour " + prenom)
